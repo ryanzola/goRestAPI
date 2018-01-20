f778c8a5e71711b6f220087eba5569a92899a6c0
ryanzola <ryanzola@me.com>
2018-01-20 14:22:22 -0500

    complete CRUD functionality

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-//structs (model)
+//structs (models)
 
 //Book struct
 type Book struct {
@@ -28,23 +31,59 @@ var books []Book
 
 //functions
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //get params
+	//loop through books, find id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // mock id, not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get params
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"] // mock id, not safe
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get params
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
@@ -55,7 +94,7 @@ func main() {
 	books = append(books, Book{ID: "1", Isbn: "44783", Title: "Book 1", Author: &Author{Firstname: "Ryan", Lastname: "Zola"}})
 	books = append(books, Book{ID: "2", Isbn: "45532", Title: "Book 2", Author: &Author{Firstname: "J.D.", Lastname: "Salinger"}})
 	books = append(books, Book{ID: "3", Isbn: "48291", Title: "Book 3", Author: &Author{Firstname: "Author", Lastname: "McBookwriter"}})
-	books = append(books, Book{ID: "3", Isbn: "48291", Title: "Book 4", Author: &Author{Firstname: "Kaijun", Lastname: "MacInauthor"}})
+	books = append(books, Book{ID: "4", Isbn: "48291", Title: "Book 4", Author: &Author{Firstname: "Kaijun", Lastname: "MacInauthor"}})
 
 	//Route handlers
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
